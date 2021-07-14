4cc0c0eef5c1a553606dd1df965d52fa0d5a6aa4
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-14 18:20:18 -0300

    Make cards, their sum and the dealer's message to display on the page.

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
+const messageEl = document.querySelector("#message-el");
+const sumEl = document.querySelector("#sum-el");
+const cardsEl = document.querySelector("#cards-el");
+
 function startGame() {
+  sumEl.textContent = `Sum: ${sum}`;
+  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
+
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
-  console.log(message);
+  messageEl.textContent = message;
 }
